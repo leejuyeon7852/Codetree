67106e78a7b37f6481ff9f9a84985c6a9b52d701
zuyo7852 <leejuyeon7852@gmail.com>
2025-09-25 00:47:23 +0900

    Title: 되돌아오기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250925/되돌아오기/come-back.py
@@ -0,0 +1,38 @@
+N = int(input())
+
+x, y = 0, 0
+MAX = 100
+arr = [[0] * MAX for _ in range(MAX)]
+
+# E-0, S-1, W-2, N-3
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+
+direction = {
+    'E': 0, #동
+    'S': 1, #남
+    'W': 2, #서
+    'N': 3 #북
+}
+
+time = 0
+stop = False
+for _ in range(N):
+    dir, dist = input().split()
+    dist = int(dist)
+    dir_num = direction[dir]
+    nx, ny = 0, 0
+    for _ in range(dist):
+        time += 1
+        x, y = x+dx[dir_num], y+dy[dir_num]
+        if x == 0 and y == 0:
+            stop = True
+            break;
+        #print(f'x:{x}, y:{y}, time: {time}')
+    if stop:
+        break;
+    else:
+        time = -1
+
+print(time)
+
+
