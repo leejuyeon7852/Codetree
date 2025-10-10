503629368d7f1af4981460e4212d12998d9b8696
zuyo7852 <leejuyeon7852@gmail.com>
2025-10-10 23:04:24 +0900

    Title: 두 사람, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251010/두 사람/two-person.py
+++ 251010/두 사람/two-person.py
@@ -4,7 +4,9 @@ age_s, gender_s = input().split()
 age_f = int(age_f)
 age_s = int(age_s)
 
-if (age_f >= 19 or age_s >= 19) and (gender_f == 'M' or gender_s == 'M'):
+if gender_f == 'M' and age_f >= 19:
+    print(1)
+elif gender_s == 'M' and age_s >= 19:
     print(1)
 else:
     print(0)
