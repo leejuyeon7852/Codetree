4226ed3c4860e5da88ec08d6168e754d58d42351
zuyo7852 <leejuyeon7852@gmail.com>
2025-11-02 23:15:03 +0900

    Title: Carry 피하기 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251102/Carry 피하기 2/escaping-carry-2.py
+++ 251102/Carry 피하기 2/escaping-carry-2.py
@@ -1,23 +1,23 @@
-import sys
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
+def is_carry(a, b, c):
+    while a > 0 or b > 0 or c > 0:
+        if a % 10 + b % 10 + c % 10 >= 10:
+            return False
+        a //= 10
+        b //= 10
+        c //= 10
+    return True
 
-max_sum = -sys.maxsize
-total = 0
 
+max_sum = -1
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            carry = False
-            if (arr[i] % 100 // 10 + arr[j] % 100 // 10 + arr[k] % 100 // 10 >= 10):
-                carry = True
-            elif (arr[i] % 10 + arr[j] % 10 + arr[k] % 10 >= 10):
-                carry = True
-            if not carry:
+            if is_carry(arr[i], arr[j], arr[k]):
                 total = arr[i] + arr[j] + arr[k]
-                #print(f'arr[i]:{arr[i]}, arr[j]:{arr[j]}, arr[k]:{arr[k]}')
-                #print(f'total: {total}')
-            max_sum = max(max_sum, total)
-
+                # print(f'arr[i]:{arr[i]}, arr[j]:{arr[j]}, arr[k]:{arr[k]}')
+                # print(f'total: {total}')
+                max_sum = max(max_sum, total)            
 print(max_sum)
