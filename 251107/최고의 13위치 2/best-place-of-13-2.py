a6ffbc193a4b3b6f70b8eeec1fdb8516df78c126
zuyo7852 <leejuyeon7852@gmail.com>
2025-11-07 22:52:30 +0900

    Title: 최고의 13위치 2, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 251107/최고의 13위치 2/best-place-of-13-2.py
+++ 251107/최고의 13위치 2/best-place-of-13-2.py
@@ -1,23 +1,25 @@
-import sys
-
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 max_count = 0
 for i in range(n):
-    for j in range(n):
-        count = 0
+    for j in range(n-2):
+        count1 = 0
+        # 첫 번째 행
         for k in range(j, j+3):
-            if k < n and arr[i][k] == 1:
-                count += 1
-                max_count = max(max_count, count)
-            if k + 3 < n and arr[i][k+3] == 1:
-                count += 1
-                max_count = max(max_count, count)
-        for k in range(j, j+3):
-            if k < n and i+1 < n and arr[i+1][k] == 1:
-                count += 1
-                max_count = max(max_count, count)
+            if arr[i][k] == 1:
+                count1 += 1
+        # 두 번째 행
+        for a in range(i, n):
+            for b in range(n-2):
+                if a == i and not (b > j + 2 or j > b + 2):
+                    continue
+                count2 = 0
+                for k in range(b, b+3):
+                    if arr[a][k] == 1:
+                        count2 += 1
+                total = count1 + count2
+                max_count = max(max_count, total)
             
 print(max_count)
 
