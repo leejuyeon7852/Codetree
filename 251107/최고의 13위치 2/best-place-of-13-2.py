d7d20ea3f750f86d9c12a3d91acbacb613b16618
zuyo7852 <leejuyeon7852@gmail.com>
2025-11-07 21:53:45 +0900

    Title: 최고의 13위치 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251107/최고의 13위치 2/best-place-of-13-2.py
+++ 251107/최고의 13위치 2/best-place-of-13-2.py
@@ -11,10 +11,14 @@ for i in range(n):
             if k < n and arr[i][k] == 1:
                 count += 1
                 max_count = max(max_count, count)
+            if k + 3 < n and arr[i][k+3] == 1:
+                count += 1
+                max_count = max(max_count, count)
         for k in range(j, j+3):
             if k < n and i+1 < n and arr[i+1][k] == 1:
                 count += 1
                 max_count = max(max_count, count)
+            
 print(max_count)
 
         
