dc3cea362380c72e5d7f123658b4fc2c867c52b1
zuyo7852 <leejuyeon7852@gmail.com>
2025-11-09 23:01:16 +0900

    Title: 아름다운 수열 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251109/아름다운 수열 2/beautiful-sequence-2.py
+++ 251109/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,14 +2,14 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+sorted_B = sorted(B)
 
 count = 0
 for i in range(N-M+1):
-    cnt = 0
-    for j in range(i, M+i):
-        if A[j] in B:
-            cnt += 1
-        if cnt == M:
-            count += 1
+    sub_A = A[i: i+M]
+    sorted_A = sorted(sub_A)
+
+    if sorted_A == sorted_B:
+        count += 1
 print(count)
 
