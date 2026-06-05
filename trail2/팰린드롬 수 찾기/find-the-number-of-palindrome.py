67abbefbaaa728266b0af5cbc3c6288ccaec9c52
zuyo7852 <leejuyeon7852@gmail.com>
2026-06-05 20:23:28 +0900

    [Passed] 팰린드롬 수 찾기 (77ms, 17MB)

--- trail2/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ trail2/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -4,7 +4,7 @@ count = 0
 
 for n in range(X, Y+1):
     flag = True
-    n = list(map(int, str(n)))
+    n = list(map(int, str(n))) # 굳이 정수로 또 바꾸 필요 없음
     for i in range(len(n)//2):
         if n[i] != n[len(n)-1-i]:
             flag = False
@@ -13,4 +13,12 @@ for n in range(X, Y+1):
         count += 1
 
 print(count)
+
+# count = 0
+# for i in range(X, Y+1):
+#     str_i = str(i)
+
+#     if str_i == str_i[::-1]:
+#         ans += 1
+# print(ans)
                    
