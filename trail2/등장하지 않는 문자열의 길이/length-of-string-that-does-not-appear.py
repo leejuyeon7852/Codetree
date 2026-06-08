4f127b76df86409d7e87eae39567c9b3364b7457
zuyo7852 <leejuyeon7852@gmail.com>
2026-06-08 14:34:29 +0900

    [Passed] 등장하지 않는 문자열의 길이 (56ms, 17MB)

--- trail2/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ trail2/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -7,13 +7,10 @@ for L in range(1, N+1):
     substrings = set()
     for i in range(N-L+1):
         substrings.add(str[i:i+L])
-        if len(substrings) == N-L+1:
-            min_length = min(L, min_length)
-            break
+    if len(substrings) == N-L+1:
+        print(L)
+        break
 
-print(min_length)
-
-
-
+#print(min_length)
         
         
