fe82608e92afc364cc7bad0f49726867111e16d9
zuyo7852 <leejuyeon7852@gmail.com>
2026-06-11 14:22:33 +0900

    [Passed] 수들의 최대 차 (104ms, 17MB)

---
+++ trail2/수들의 최대 차/maximum-difference-in-numbers.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+
+arr.sort()
+max_length = 0
+
+for i in range(N):
+    lst = []
+    for j in range(N):
+        if 0 <= arr[j] - arr[i] <= K:
+            lst.append(arr[j])
+    if max(lst) - min(lst) <= K:
+        max_length = max(max_length, len(lst))
+
+print(max_length)
