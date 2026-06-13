b35294cd5ce72505779c9df0b02e4cd6412e9f00
zuyo7852 <leejuyeon7852@gmail.com>
2026-06-13 14:34:30 +0900

    [Passed] 청소당번 정하기 (49ms, 16MB)

--- trail1/청소당번 정하기/cleaning-numbering.py
+++ trail1/청소당번 정하기/cleaning-numbering.py
@@ -1,28 +1,13 @@
 n = int(input())
 
-a = 0 # 교실 이틀
-b = 0 # 복도 3일
-c = 0 # 12일
+a, b, c = 0, 0, 0
 
-days = [0]*(n+1)
-
-for i in range(2, n+1, 2):
-    days[i] = 2
-
-for i in range(3, n+1, 3):
-    if days[i] <= 3:
-        days[i] = 3
-
-for i in range(12, n+1, 12):
-    if days[i] <= 12:
-        days[i] = 12
-
-for i in range(n+1):
-    if days[i] == 2:
-        a += 1
-    elif days[i] == 3:
-        b += 1
-    elif days[i] == 12:
+for i in range(1, n+1):
+    if i % 12 == 0:
         c += 1
+    elif i % 3 == 0:
+        b += 1
+    elif i % 2 == 0:
+        a += 1
 
 print(a, b, c)
