e8af66130a74a2340a9e7ec95cd6cae21c8987d1
zuyo7852 <leejuyeon7852@gmail.com>
2026-06-19 14:40:14 +0900

    [Passed] 순위 경쟁2 (52ms, 16MB)

---
+++ trail2/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,22 @@
+n = int(input())
+events = []
+for _ in range(n):
+    name, delta = input().split()
+    delta = int(delta)
+    events.append((name, delta))
+
+scores = {'A': 0, 'B': 0}
+prev_top = {'A', 'B'}  # 명예의 전당
+count = 0
+
+for name, delta in events:
+    scores[name] += delta
+
+    max_score = max(scores.values())
+    current_top = {k for k, v in scores.items() if v == max_score}
+
+    if current_top != prev_top:
+        count += 1
+        prev_top = current_top
+
+print(count)
