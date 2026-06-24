7f30fc2b81ce6f3f889ea5a1627701fa6a713423
zuyo7852 <leejuyeon7852@gmail.com>
2026-06-24 14:56:57 +0900

    [Passed] 퀵 정렬 구현 (179ms, 26MB)

---
+++ trail3/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def select_pivot(arr, low, high):
+    return arr[high]
+
+def partition(arr, low, high):
+    pivot = select_pivot(arr, low, high)
+    i = low-1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pos = partition(arr, low, high)
+        quick_sort(arr, low, pos-1)
+        quick_sort(arr, pos+1, high)
+
+
+quick_sort(arr, 0, n-1)
+print(*arr)
