a64a15b9af7b9f3ab1d01dc0a7d56c4ec1c322aa
zuyo7852 <leejuyeon7852@gmail.com>
2026-09-06 13:58:13 +0900

    [Passed] Date to Date (136ms, 10MB)

--- trail2/Date to Date/date-to-date.java
+++ trail2/Date to Date/date-to-date.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 public class Main {
+    public static int numOfDays(int m, int d){
+        // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+        int []days =  new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int totalDays = 0;
+
+        for(int i = 1; i < m; i++){
+            totalDays += days[i];
+        }
+        totalDays += d;
+
+        return totalDays;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
@@ -7,25 +22,6 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
         
-        // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-        int []num_of_days =  new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        int total_days = 0;
-        for(int i=1; i<m2; i++){
-            total_days += num_of_days[i];
-            //System.out.println(i+":"+total_days);
-        }
-        total_days += d2;
-        //System.out.println(total_days);
-
-        int sum_days = 0;
-        for(int i=1; i<m1; i++){
-            sum_days += num_of_days[i];
-            //System.out.println(i+":"+sum_days);
-        }
-        sum_days += d1;
-        //System.out.println(sum_days);
-
-        System.out.println(total_days-sum_days+1);
+        System.out.println(numOfDays(m2, d2)-numOfDays(m1, d1) + 1);
     }
 }
