a9ae31e7157551b9a41f94a5a4d2f2a210653d46
zuyo7852 <leejuyeon7852@gmail.com>
2026-09-06 14:30:58 +0900

    [Passed] DateTime to DateTime (137ms, 10MB)

--- trail2/DateTime to DateTime/datetime-to-datetime.java
+++ trail2/DateTime to DateTime/datetime-to-datetime.java
@@ -1,26 +1,16 @@
 import java.util.Scanner;
 public class Main {
-    public static int elapsedMinutes(int d, int h, int m){
-        if(d < 11){
-            return -1;
-        }else if(d == 11){
-            if (h < 11){
-                return -1;
-            }else if (h == 11 && m < 11){
-                return -1;
-            }
-        }
-
-        int minutes = ((d-11)*24*60)+((h-11)*60)+(m-11);
-        return minutes;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.println(elapsedMinutes(A, B, C));
+        int diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
+
+        if(diff < 0)
+            System.out.println(-1);
+        else
+            System.out.println(diff);
     }
 }
